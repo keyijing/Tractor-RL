99638c250e19997e13b05fc6c233427758dfc980
keyisi2006 <ke_yi_si@163.com>
2025-11-19 18:48:17 +0800

    add ID_TO_SUIT to card.py

--- card.py
+++ card.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 Card = Tuple[int, int, int]
 
+ID_TO_SUIT = ['s', 'h', 'c', 'd']
 SUIT_TO_ID = {'': None, 'n': -1, 's': 0, 'h': 1, 'c': 2, 'd': 3}
 NUMBER_TO_ID = {k: v for v, k in enumerate([
 	'2','3','4','5','6','7','8','9','10','J','Q','K','A'
